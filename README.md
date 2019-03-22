2e31d68e53eed1afe553f4553991f55d16c9c654
gyengus <gyengus@gmail.com>
2019-03-22 11:01:54 +0100

    Some changes
    - add README
    - add error handlers
    - add continuous mode
    - support multiple command line parameter
    - extend help text

---
+++ README.md
@@ -0,0 +1,16 @@
+# Show Raspberry Pi's or Orange Pi's CPU temperature
+
+It reads CPU temperature from `/sys/class/thermal/thermal_zone0/temp`.
+
+Usage: 
+```bash
+$ cputemp.py [option]
+```
+
+Options: | &nbsp;
+--- | ---:
+-h, --help | display this help and exit
+-f, --format telegraf\|n | setting output format
+-c, --continuous | refresh every 10 seconds
+
+Project page: https://github.com/gyengus/cputemp
--- cputemp.py
+++ cputemp.py
@@ -1,33 +1,68 @@
 #!/usr/bin/env python3
 
-import os, sys, getopt, array
+from os import popen
+from sys import argv, exit, stdout
+from getopt import getopt
+from time import sleep
 
 def readTemp():
-    temp = os.popen("cat /sys/class/thermal/thermal_zone0/temp").readline()
-    temp = int(temp.replace("\n", ""))
-    return (temp)
+    try:
+        file = open("/sys/class/thermal/thermal_zone0/temp", "r")
+        temp = int(file.read().replace("\n", ""))
+        file.close()
+        return(temp)
+    except:
+        print("Unsupported device")
+        exit()
 
 def convertTemp(temp):
     if temp > 200:
         return (temp / 1000)
-    return (temp)
+    return(temp)
 
 def showHelp():
-    print("Show Raspberry Pi's or Orange Pi's CPU temperature\nUsage: cputemp [option]\n\nOptions:\n\t-h, --help\t\t\tdisplay this help\n\t-f, --format telegraf|n\t\tsetting output format")
+    print("Show Raspberry Pi's or Orange Pi's CPU temperature\n"
+        + "Usage: cputemp [option]\n\n"
+        + "Options:\n"
+        + "\t-h, --help\t\t\tdisplay this help and exit\n"
+        + "\t-f, --format telegraf|n\t\tsetting output format\n"
+        + "\t-c, --continuous\t\trefresh every 10 seconds\n\n"
+        + "Project page: https://github.com/gyengus/cputemp")
+    exit()
 
+def formatTemp(arg):
+    if arg == "telegraf":
+        return("cpu temperature=" + str(temp))
+    elif arg == "n":
+        return(str(temp) + " °C")
+    else:
+        return("CPU temperature: " + str(temp) + " °C")
 
-temp = convertTemp(readTemp())
-argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, "f:h", ["format=", "help"])
-if len(opts) == 0:
-    opts = [("-f", "n")]
-for opt, arg in opts:
-    if opt in ("-f", "--format"):
-        if arg == "telegraf":
-            print("cpu temperature=" + str(temp))
-        elif arg == "n":
-            print(str(temp) + " °C")
-        else:
-            print(str(temp))
-    elif opt in ("-h", "--help"):
-        showHelp()
+try:
+    argv = argv[1:]
+    opts, args = getopt(argv, "f:hc", ["format=", "help", "continuous"])
+    format = "h"
+    continuous = False
+    if len(opts) == 0:
+        opts = [("-f", "h")]
+    for opt, arg in opts:
+        if opt in ("-f", "--format"):
+            format = arg
+        elif opt in ("-h", "--help"):
+            showHelp()
+        elif opt in ("-c", "--continuous"):
+            continuous = True
+
+    while True:
+        temp = convertTemp(readTemp())
+        if continuous:
+            stdout.write(u"\u001b[1000D")
+        stdout.write(formatTemp(format))
+        stdout.flush()
+        if not continuous:
+            break;
+        sleep(10)
+    print("")
+except KeyboardInterrupt:
+    print("")
+    exit()
